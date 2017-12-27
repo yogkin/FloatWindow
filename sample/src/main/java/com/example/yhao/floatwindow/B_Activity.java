7d9b045b900277916c1a48d512ab6cfbd24004cd
czm <490277070@qq.com>
2017-12-27 10:01:46 +0800

    降低最低SDK为17，添加部分图标

--- sample/src/main/java/com/example/yhao/floatwindow/B_Activity.java
+++ sample/src/main/java/com/example/yhao/floatwindow/B_Activity.java
@@ -3,6 +3,8 @@ package com.example.yhao.floatwindow;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.yhao.fixedfloatwindow.R;
 
@@ -14,6 +16,10 @@ public class B_Activity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
         setTitle("B");
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.float_item_select);
+
+        addContentView(imageView,new RelativeLayout.LayoutParams(100,100));
     }
 
     public void change(View view) {
--- sample/src/main/java/com/example/yhao/floatwindow/BaseApplication.java
+++ sample/src/main/java/com/example/yhao/floatwindow/BaseApplication.java
@@ -1,6 +1,8 @@
 package com.example.yhao.floatwindow;
 
 import android.app.Application;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
 
@@ -21,19 +23,28 @@ public class BaseApplication extends Application {
         super.onCreate();
 
         ImageView imageView = new ImageView(getApplicationContext());
-        imageView.setImageResource(R.drawable.icon);
+//        imageView.setImageResource(R.drawable.icon);
+        imageView.setImageResource(R.drawable.float_select01);
+
 
         //效果图1
         FloatWindow
                 .with(getApplicationContext())
                 .setView(imageView)
-                .setWidth(Screen.width,0.2f)
-                .setHeight(Screen.width,0.2f)
+                .setWidth(Screen.width,0.1f)
+                .setHeight(Screen.width,0.1f)
                 .setX(Screen.width,0.8f)
                 .setY(Screen.height,0.3f)
                 .setMoveType(MoveType.slide)
                 .setMoveStyle(500,new BounceInterpolator())
                 .build();
+
+        FloatWindow.get().getView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BaseApplication", "点击了");
+            }
+        });
 
         //效果图2
 //        FloatWindow
